f2a0aa20ea0491f802a40d9f03cf3707d42b07fd
Matt Rice <matthewcrice32@gmail.com>
2021-03-30 21:55:54 -0400

    feat: make TVL dynamic and fix the How it works button (#74)

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,21 @@
 /*
  * Polyfills
  */
-import './polyfills/object-fit';
+import "./polyfills/object-fit";
 
 /*
  * Modules
  */
 
-import './modules/handle-mobile-nav';
-import './modules/handle-scroll';
-import './modules/handle-select-box';
-import './modules/handle-filter';
-import './modules/handle-scroll-other-page';
+import "./modules/handle-mobile-nav";
+import "./modules/handle-scroll";
+import "./modules/handle-select-box";
+import "./modules/handle-filter";
+import "./modules/handle-scroll-other-page";
 
-import handleFormSubmit from './modules/handle-form-submit.js';
+import handleFormSubmit from "./modules/handle-form-submit.js";
+import fetchTvl from "./modules/fetch-tvl";
 
-handleFormSubmit()
+handleFormSubmit();
+const fiveMinutesInMilliseconds = 1000 * 60 * 5;
+setInterval(() => fetchTvl(), fiveMinutesInMilliseconds);
