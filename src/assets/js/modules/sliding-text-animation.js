8e74793f3fe490307d0049a39aa6ca9d5a0097b9
Tulun <jaykiraly@gmail.com>
2021-08-19 15:20:11 -0700

    saving progress.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/assets/js/modules/sliding-text-animation.js
+++ src/assets/js/modules/sliding-text-animation.js
@@ -1,34 +1,7 @@
 import { $document, $window } from "../utils/globals";
 
-console.log($document, $window);
+// Sliding animation for kpi-options page
 $document.ready(() => {
-  console.log("hello?");
-  // console.log("event", event);
-  // function roll() {
-  //   var curr = 2;
-  //   var count = 4;
-  //   $("#testimonials .testimonial:nth-child(1)").show(
-  //     "slide",
-  //     { direction: "down" },
-  //     500
-  //   );
-  //   setInterval(function () {
-  //     $("#testimonials .testimonial").hide("slide", { direction: "up" }, 500);
-  //     $("#testimonials .testimonial:nth-child(" + curr + ")")
-  //       .delay(750)
-  //       .show("slide", { direction: "down" }, 500);
-  //     curr++;
-  //     if (curr == count + 1) {
-  //       // 5000 - 500 - 750 - 500 = 3250    =>    delay = 3000
-  //       $("#testimonials .testimonial:nth-child(4)")
-  //         .delay(3000)
-  //         .hide("slide", { direction: "up" }, 500);
-  //       curr = 1;
-  //     }
-  //   }, 5000);
-  // }
-  // roll();
-
   function slide() {
     var curr = 0;
 
@@ -38,7 +11,6 @@ $document.ready(() => {
       );
 
       curr += 1;
-      console.log("curr", curr);
       // start
       if (curr === 7) {
         curr = 0;
