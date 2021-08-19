fe4e01907987d78ace0e4f2fc268ccea1aec9262
Tulun <jaykiraly@gmail.com>
2021-08-19 13:35:41 -0700

    animation works; css slightly off.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/assets/js/modules/sliding-text-animation.js
+++ src/assets/js/modules/sliding-text-animation.js
@@ -1,30 +1,50 @@
 import { $document, $window } from "../utils/globals";
 
-console.log("loading?");
-$(".kpi").on("mouseover", (event) => {
-  console.log("event", event);
-  function roll() {
-    var curr = 2;
-    var count = 4;
-    $("#testimonials .testimonial:nth-child(1)").show(
-      "slide",
-      { direction: "down" },
-      500
-    );
+console.log($document, $window);
+$document.ready(() => {
+  console.log("hello?");
+  // console.log("event", event);
+  // function roll() {
+  //   var curr = 2;
+  //   var count = 4;
+  //   $("#testimonials .testimonial:nth-child(1)").show(
+  //     "slide",
+  //     { direction: "down" },
+  //     500
+  //   );
+  //   setInterval(function () {
+  //     $("#testimonials .testimonial").hide("slide", { direction: "up" }, 500);
+  //     $("#testimonials .testimonial:nth-child(" + curr + ")")
+  //       .delay(750)
+  //       .show("slide", { direction: "down" }, 500);
+  //     curr++;
+  //     if (curr == count + 1) {
+  //       // 5000 - 500 - 750 - 500 = 3250    =>    delay = 3000
+  //       $("#testimonials .testimonial:nth-child(4)")
+  //         .delay(3000)
+  //         .hide("slide", { direction: "up" }, 500);
+  //       curr = 1;
+  //     }
+  //   }, 5000);
+  // }
+  // roll();
+
+  function slide() {
+    var curr = 0;
+
     setInterval(function () {
-      $("#testimonials .testimonial").hide("slide", { direction: "up" }, 500);
-      $("#testimonials .testimonial:nth-child(" + curr + ")")
-        .delay(750)
-        .show("slide", { direction: "down" }, 500);
-      curr++;
-      if (curr == count + 1) {
-        // 5000 - 500 - 750 - 500 = 3250    =>    delay = 3000
-        $("#testimonials .testimonial:nth-child(4)")
-          .delay(3000)
-          .hide("slide", { direction: "up" }, 500);
-        curr = 1;
+      $(`#kpi-cta-container .kpi-hero-cta:nth-of-type(${curr})`).slideUp(
+        "slow"
+      );
+
+      curr += 1;
+      console.log("curr", curr);
+      // start
+      if (curr === 7) {
+        curr = 0;
+        $(`#kpi-cta-container .kpi-hero-cta`).show("slow");
       }
-    }, 5000);
+    }, 3000);
   }
-  roll();
+  // slide();
 });
