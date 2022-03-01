963818b5b40705beef44d07f8143ea4b20e882bc
amateima <amatei@umaproject.org>
2022-03-01 02:02:43 +0200

    Add products page
    Signed-off-by: amateima <amatei@umaproject.org>

---
+++ pages/products.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import { HomePage } from '../components/HomePage'
+import { ProductsPage } from '../components/ProductsPage';
+
+const ProductsRoute: NextPage = () => {
+  return (
+    <ProductsPage />
+  )
+}
+
+export default ProductsRoute;
