7c090808c6599d8b26a1156a698c7ce7ea6de686
amateima <amatei@umaproject.org>
2022-03-01 02:24:28 +0200

    Remove G Analytics
    Signed-off-by: amateima <amatei@umaproject.org>

--- components/SeoHead/index.tsx
+++ components/SeoHead/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from "next/head";
-import Script from 'next/script'
 
 interface Props {
   title?: string,
@@ -31,20 +30,6 @@ export default function SeoHead(props: Props) {
       <link rel="icon" type="image/png" href="assets/images/favicon-32x32.png" sizes="32x32" />
 	    <link rel="icon" type="image/png" href="assets/images/favicon-16x16.png" sizes="16x16" />
       <link rel="mask-icon" href="assets/images/safari-pinned-tab.svg" color="#FF4A4A" />
-
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=UA-130599982-1"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-130599982-1');
-        `}
-      </Script>
     </Head>
   );
 }
