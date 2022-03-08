04208bbd8180784e69c27dbeace8af9353a2b31d
amateima <amatei@umaproject.org>
2022-03-09 00:20:27 +0200

    Add missing icons
    Signed-off-by: amateima <amatei@umaproject.org>

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const a: () => void = () => {
-
-}
-
 export const UmaLogoIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 137 35" width={137} height={35} {...props}>
@@ -20,6 +16,14 @@ export const UmaLogoIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactE
   );
 }
 
+export const DiagonalArrowIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 8" width={8} height={8} {...props}>
+      <path fill="currentColor" d="M7.49998 1c0-.276134-.22385-.499985-.49998-.499984L2.50013.500015C2.224.500016 2.00015.723866 2.00015 1c0 .27613.22385.49999.49998.49999h3.99988v3.99988c0 .27613.22386.49998.49999.49998s.49998-.22385.49998-.49998V1ZM1.35354 7.35354l6-6-.70708-.707082L.646457 6.64646l.707083.70708Z" />
+    </svg>
+  );
+}
+
 export const ArrowRightTailIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 12.4 10.8" width={12.4} height={10.8} {...props}>
@@ -32,6 +36,115 @@ export const DownIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElem
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 5" width={8} height={5} {...props}>
       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m1 1 3 3 3-3" />
+    </svg>
+  );
+}
+
+export const KpiOptionIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 60 60" width={60} height={60} {...props}>
+      <path fill="#FF4A4A" d="M30 60c16.5685 0 30-13.4315 30-30S46.5685 0 30 0 0 13.4315 0 30s13.4315 30 30 30Z" />
+      <path fill="#fff" d="M29.7017 34.5812c2.3378 0 4.233-1.8952 4.233-4.233 0-2.3378-1.8952-4.233-4.233-4.233-2.3378 0-4.2329 1.8952-4.2329 4.233 0 2.3378 1.8951 4.233 4.2329 4.233Z" />
+      <path stroke="#fff" strokeWidth="2.14286" d="M29.7895 45.5166c8.4259 0 15.2564-6.8305 15.2564-15.2564 0-8.4258-6.8305-15.2563-15.2564-15.2563-8.4258 0-15.2563 6.8305-15.2563 15.2563 0 8.4259 6.8305 15.2564 15.2563 15.2564Z" />
+      <path stroke="#fff" strokeWidth="2.14286" d="M29.7906 39.584c5.1493 0 9.3237-4.1743 9.3237-9.3237 0-5.1494-4.1744-9.3238-9.3237-9.3238-5.1494 0-9.3238 4.1744-9.3238 9.3238s4.1744 9.3237 9.3238 9.3237Z" />
+      <circle cx="29.5" cy="30.5" r="2.5" fill="#FF4A4A" />
+      <mask id="a" width="21" height="22" x="28" y="10" maskUnits="userSpaceOnUse" style={{ maskType: "alpha" }}>
+        <path fill="#fff" d="M48.8559 10.5098H28.3965v21.1648h20.4594V10.5098Z" />
+        <path fill="#000" fillRule="evenodd" d="m42.909 15.7654-.1443-1.4863c-.0203-.2094.0544-.4169.2037-.5657l1.5918-1.5859c.3766-.3752 1.0186-.2164 1.1752.2908l.3455 1.1184c.0697.2259.2485.4018.476.4685l1.0508.3079c.5146.1508.6797.7956.3003 1.1736l-1.5844 1.5785c-.1467.1463-.3506.2208-.5575.2039l-1.3847-.1131-13.6369 13.5869c-.146.1456-.3486.2202-.5546.2042l-.4031-.0312c-.3471-.027-.6228-.3017-.6498-.6475l-.0314-.4016c-.016-.2052.0589-.4071.2049-.5526L42.909 15.7654Z" clipRule="evenodd" />
+      </mask>
+      <g mask="url(#a)">
+        <path fill="#fff" stroke="#FF4A4A" strokeLinejoin="round" strokeWidth="1.2" d="m47.908 15.4867-1.5844 1.5785c-.1467.1463-.3506.2208-.5575.2039l-1.3847-.1131-13.6369 13.5869M47.908 15.4867 30.1899 30.9471c.206.016.4086-.0586.5546-.2042M47.908 15.4867c.3794-.378.2143-1.0228-.3003-1.1736l.3003 1.1736ZM30.7445 30.7429l.4235.4251.0001-.0001m-.4236-.425.4236.425m0 0 13.4405-13.3913 1.1086.0905h.0001c.3817.0312.7583-.1063 1.0298-.3768 0-.0001 0-.0001.0001-.0001l1.5843-1.5785c.7039-.7014.3954-1.8958-.5551-2.1744l-1.0508-.3079c-.035-.0103-.0613-.037-.0714-.0696l-.0001-.0002-.3454-1.1183c-.2904-.9404-1.4771-1.2313-2.172-.5388l-1.5917 1.5858c-.2765.2754-.4152.6601-.3775 1.0488l.1169 1.2037L28.887 28.8891v.0001c-.2702.2693-.4092.6434-.3795 1.0242v.0001l.0313.4016v.0001c.0502.642.5615 1.1492 1.2016 1.1989l.4031.0312h.0001c.3799.0294.7542-.1081 1.0245-.3774Z" />
+      </g>
+    </svg>
+  );
+}
+
+export const SuccessTokenIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60" width={60} height={60} {...props}>
+      <g clipPath="url(#clip0_1370_2856)">
+        <path d="M30.2488 60C46.6517 60 59.9488 46.5685 59.9488 30C59.9488 13.4315 46.6517 0 30.2488 0C13.846 0 0.548828 13.4315 0.548828 30C0.548828 46.5685 13.846 60 30.2488 60Z" fill="#FF4A4A" />
+      </g>
+      <path d="M16.3887 43.4004H44.3067" stroke="white" strokeWidth="2.1" strokeLinecap="round" />
+      <path d="M30.3484 38.2C36.4174 38.2 41.3374 33.2304 41.3374 27.1C41.3374 20.9696 36.4174 16 30.3484 16C24.2793 16 19.3594 20.9696 19.3594 27.1C19.3594 33.2304 24.2793 38.2 30.3484 38.2Z" fill="#FF4A4A" stroke="white" strokeWidth="2.1" />
+      <path d="M26.7832 27.2161L29.7724 30.7L34.5052 23.5" stroke="white" strokeWidth="2.1" strokeLinecap="round" strokeLinejoin="round" />
+      <defs>
+        <clipPath id="clip0_1370_2856">
+          <rect width="59.4" height="60" fill="white" transform="translate(0.548828)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export const RangeTokenIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg" width={60} height={60} {...props}>
+      <g clipPath="url(#clip0_1022_31454)">
+        <path d="M30 60C46.5685 60 60 46.5685 60 30C60 13.4315 46.5685 0 30 0C13.4315 0 0 13.4315 0 30C0 46.5685 13.4315 60 30 60Z" fill="#FF4A4A" />
+        <path d="M0 29.6998C1.66667 28.4998 3.33334 28.4998 5 29.6998C6.66666 30.8998 8.33334 30.8998 10 29.6998C11.6666 28.4998 13.3333 28.4998 15 29.6998C16.6667 30.8998 18.3334 30.8998 20 29.6998C21.6667 28.4998 23.3333 28.4998 25 29.6998C26.6666 30.8998 28.3333 30.8998 30 29.6998C31.6667 28.4998 33.3334 28.4998 35 29.6998C36.6667 30.8998 38.3333 30.8998 40 29.6998C41.6666 28.4998 43.3333 28.4998 45 29.6998C46.6667 30.8998 48.3334 30.8998 50 29.6998C51.6667 28.4998 53.3333 28.4998 55 29.6998C56.6666 30.8998 58.3333 30.8998 60 29.6998" stroke="url(#paint0_linear_1022_31454)" strokeWidth="1.5" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M42.931 30.6224L45.9924 31.133L44.9257 35.2759L44.9235 35.2755C44.093 39.1405 36.6857 41.1239 28.2188 39.7116C19.7519 38.2993 13.3894 34.019 13.8583 30.0938L13.8553 30.0932L14.1909 25.8285L17.2532 26.3393C20.3666 24.9118 25.2808 24.4535 30.6154 25.3434C35.9501 26.2332 40.4495 28.2616 42.931 30.6224Z" fill="url(#paint1_linear_1022_31454)" />
+        <path d="M28.908 35.5708C37.6898 37.0356 45.3594 34.9224 46.0385 30.8507C46.7177 26.7791 40.1492 22.2909 31.3675 20.8261C22.5857 19.3613 14.9162 21.4745 14.237 25.5462C13.5579 29.6178 20.1263 34.106 28.908 35.5708Z" fill="white" />
+        <path d="M29.1313 34.2324C36.7945 35.5106 43.4682 33.7812 44.0372 30.3696C44.6063 26.9581 38.8553 23.1562 31.192 21.878C23.5287 20.5997 16.8551 22.3291 16.286 25.7407C15.717 29.1523 21.468 32.9541 29.1313 34.2324Z" fill="#FF5E5E" />
+      </g>
+      <defs>
+        <linearGradient id="paint0_linear_1022_31454" x1="-1.94444" y1="30.5998" x2="59.1667" y2="30.5998" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" stopOpacity="0" />
+          <stop offset="0.239583" stopColor="white" stopOpacity="0.6" />
+          <stop offset="0.5" stopColor="white" />
+          <stop offset="0.78125" stopColor="white" stopOpacity="0.6" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_1022_31454" x1="20.9999" y1="30.0001" x2="41.8133" y2="34.6018" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="#FDE7E7" />
+        </linearGradient>
+        <clipPath id="clip0_1022_31454">
+          <rect width="60" height="60" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export const OOLogoIcon = ({ style = {}, ...rest }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 60 60" style={{ width: 60, height: 60, ...style }} {...rest}>
+      <circle cx="30" cy="30" r="30" fill="#FF4A4A" />
+      <rect width="38.1353" height="21.7041" x="10.9309" y="19.1437" fill="#FF4A4A" stroke="#fff" strokeWidth="2.1" rx="10.8521" />
+      <circle cx="22.4749" cy="29.8186" r="5.4425" stroke="#fff" strokeWidth="2.14286" />
+      <circle cx="37.5296" cy="29.8186" r="5.4425" stroke="#fff" strokeWidth="2.14286" />
+    </svg>
+  );
+}
+
+export const CheckmarkFilledIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 15 15" width={15} height={15} {...props}>
+      <circle cx="7.5" cy="7.5" r="7.5" fill="#272528" />
+      <path fill="#272528" d="M5 7.58063 6.9355 10 10 5" />
+      <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.4" d="M5 7.58063 6.9355 10 10 5" />
+    </svg>
+  );
+}
+
+export const CallPutOptionsIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 60 60" width={15} height={15} {...props}>
+      <circle cx="30" cy="30" r="30" fill="#FF4A4A" />
+      <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.1" d="m13.0369 42.6843 9.5485-11.2845c1.6102-1.903 4.5814-1.7811 6.0304.2474 1.4706 2.0589 4.4995 2.1474 6.0878.1778l11.2965-14.0076" />
+    </svg>
+  );
+}
+
+export const LspIcon: (props: React.SVGProps<SVGSVGElement>) => React.ReactElement = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 60 60" width={15} height={15} {...props}>
+      <circle cx="30" cy="30" r="30" fill="#FF4A4A" />
+      <path stroke="#fff" strokeWidth="2.1" d="M39 36c3.3137 0 6-2.6863 6-6s-2.6863-6-6-6-6 2.6863-6 6 2.6863 6 6 6Z" />
+      <path stroke="#fff" strokeLinecap="round" strokeWidth="2.1" d="M39 15v8" />
+      <path stroke="#fff" strokeWidth="2.1" d="M21 36c3.3137 0 6-2.6863 6-6s-2.6863-6-6-6-6 2.6863-6 6 2.6863 6 6 6Z" />
+      <path stroke="#fff" strokeLinecap="round" strokeWidth="2.1" d="M21 37v8" />
     </svg>
   );
 }
