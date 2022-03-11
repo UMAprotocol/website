71e58fcd55df9784aa17b42581f82e52ba1dad18
Alex Gaines <againes@umaproject.org>
2022-03-11 12:52:43 -0600

    Update TVL endpoint
    Signed-off-by: Alex Gaines <againes@umaproject.org>

--- src/assets/js/modules/fetch-tvl.js
+++ src/assets/js/modules/fetch-tvl.js
@@ -1,4 +1,4 @@
-const tvlUrl = "https://api.llama.fi/tvl/uma-protocol";
+const tvlUrl = "https://api.llama.fi/oracles";
 const format = Intl.NumberFormat("en-US", {
   currency: "USD",
   style: "currency",
@@ -9,6 +9,7 @@ const format = Intl.NumberFormat("en-US", {
 function fetchTvl() {
   fetch(tvlUrl)
     .then((res) => res.json())
+    .then((data) => data.chart[Object.keys(data.chart)[Object.keys(data.chart).length-1]].UMA.tvl)
     .then((tvl) => format(tvl / 10 ** 6))
     .then((formattedTvl) => {
       console.log({ formattedTvl });
