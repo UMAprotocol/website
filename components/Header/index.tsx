82db80422fcbbb40f9f491569f22baef2992d8b6
Jason Kiraly <jaykiraly@gmail.com>
2022-05-14 09:43:18 -0700

    added link (#147)
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { constants } from "../../utils";
-import { DownIcon } from "../Icons"
-import * as UI from "./components"
-import Image from 'next/image'
+import { DownIcon } from "../Icons";
+import * as UI from "./components";
+import Image from "next/image";
 import { useState } from "react";
 
 const { COMMUNITY_LINKS } = constants;
@@ -14,18 +14,20 @@ const NavLinks = () => {
   return (
     <UI.LinkList>
       {HEADER_LINKS.map((link) => (
-        <UI.LinkListItem key={link.key}>{link.component({ path: router.pathname })}</UI.LinkListItem>
+        <UI.LinkListItem key={link.key}>
+          {link.component({ path: router.pathname })}
+        </UI.LinkListItem>
       ))}
     </UI.LinkList>
   );
-}
+};
 
 export const Header: React.FunctionComponent = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   const onToggle = () => {
     setShowMobileMenu((prevShowMobileMenu) => !prevShowMobileMenu);
-  }
+  };
 
   return (
     <UI.Container>
@@ -41,24 +43,29 @@ export const Header: React.FunctionComponent = () => {
       </UI.Content>
     </UI.Container>
   );
-}
+};
 
-const MobileMenuComponent: React.FC<{ show: boolean, onClickLink: () => void }> = ({ show, onClickLink }) => {
+const MobileMenuComponent: React.FC<{
+  show: boolean;
+  onClickLink: () => void;
+}> = ({ show, onClickLink }) => {
   const router = useRouter();
 
   return (
     <UI.MobileMenuContainer show={show}>
-        {MOBILE_HEADER_LINKS.map((link) => link.component({ path: router.pathname, onClick: onClickLink }))}
-        <UI.MobileCommunityLinks>
-          {COMMUNITY_LINKS.map((link, idx) => (
-            <UI.MobileCommunityLink key={idx} href={link.href} target="_blank">
-              <Image src={link.iconSrc} alt={link.alt} width={25} height={25} />
-            </UI.MobileCommunityLink>
-          ))}
-        </UI.MobileCommunityLinks>
+      {MOBILE_HEADER_LINKS.map((link) =>
+        link.component({ path: router.pathname, onClick: onClickLink })
+      )}
+      <UI.MobileCommunityLinks>
+        {COMMUNITY_LINKS.map((link, idx) => (
+          <UI.MobileCommunityLink key={idx} href={link.href} target="_blank">
+            <Image src={link.iconSrc} alt={link.alt} width={25} height={25} />
+          </UI.MobileCommunityLink>
+        ))}
+      </UI.MobileCommunityLinks>
     </UI.MobileMenuContainer>
   );
-}
+};
 
 const MenuToggle: React.FC<{ toggled: boolean; onToggle: () => void }> = ({
   toggled,
@@ -88,24 +95,53 @@ const CommunityDropdown: React.FunctionComponent = () => {
               <span>{link.name}</span>
             </UI.CommunityLink>
           ))}
+          <UI.CommunityLink
+            href="https://cryptojobslist.com/companies/uma"
+            target="_blank"
+          >
+            <a
+              href="https://cryptojobslist.com/companies/uma"
+              title="We are hiring!"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                src="https://cryptojobslist.com/images/logo-circle-color.svg"
+                width="24px"
+                height="24px"
+                alt="We are hiring!"
+              />
+            </a>
+            <span>We are hiring!</span>
+          </UI.CommunityLink>
         </UI.CommunityLinks>
       </UI.DropdownValuesContainer>
     </UI.CommunityDropdownContainer>
   );
-}
+};
 
 const HEADER_LINKS = [
   {
     key: "Projects",
-    component: () => <UI.NavLink href="https://projects.umaproject.org/">Projects</UI.NavLink>,
+    component: () => (
+      <UI.NavLink href="https://projects.umaproject.org/">Projects</UI.NavLink>
+    ),
   },
   {
     key: "Products",
-    component: ({ path }: { path: string }) => <UI.NavLink href="/products" active={path === "/products"}>Products</UI.NavLink>,
+    component: ({ path }: { path: string }) => (
+      <UI.NavLink href="/products" active={path === "/products"}>
+        Products
+      </UI.NavLink>
+    ),
   },
   {
     key: "Docs",
-    component: () =><UI.NavLink href="https://docs.umaproject.org/" target="_blank">Docs</UI.NavLink>,
+    component: () => (
+      <UI.NavLink href="https://docs.umaproject.org/" target="_blank">
+        Docs
+      </UI.NavLink>
+    ),
   },
   {
     key: "Community",
@@ -113,7 +149,9 @@ const HEADER_LINKS = [
   },
   {
     key: "Vote",
-    component: () => <UI.NavLink href="https://vote.umaproject.org/">Vote</UI.NavLink>,
+    component: () => (
+      <UI.NavLink href="https://vote.umaproject.org/">Vote</UI.NavLink>
+    ),
   },
 ];
 
@@ -124,18 +162,38 @@ interface IHeaderLink {
 const MOBILE_HEADER_LINKS: IHeaderLink[] = [
   {
     key: "Projects",
-    component: () => <UI.MobileNavLink href="https://projects.umaproject.org/">Projects</UI.MobileNavLink>,
+    component: () => (
+      <UI.MobileNavLink href="https://projects.umaproject.org/">
+        Projects
+      </UI.MobileNavLink>
+    ),
   },
   {
     key: "Products",
-    component: ({ path, onClick }) => <UI.MobileNavLink href="/products" active={path === "/products"} onClick={onClick}>Products</UI.MobileNavLink>,
+    component: ({ path, onClick }) => (
+      <UI.MobileNavLink
+        href="/products"
+        active={path === "/products"}
+        onClick={onClick}
+      >
+        Products
+      </UI.MobileNavLink>
+    ),
   },
   {
     key: "Docs",
-    component: () =><UI.MobileNavLink href="https://docs.umaproject.org/" target="_blank">Docs</UI.MobileNavLink>,
+    component: () => (
+      <UI.MobileNavLink href="https://docs.umaproject.org/" target="_blank">
+        Docs
+      </UI.MobileNavLink>
+    ),
   },
   {
     key: "Vote",
-    component: () => <UI.MobileNavLink href="https://vote.umaproject.org/">Vote</UI.MobileNavLink>,
+    component: () => (
+      <UI.MobileNavLink href="https://vote.umaproject.org/">
+        Vote
+      </UI.MobileNavLink>
+    ),
   },
 ];
